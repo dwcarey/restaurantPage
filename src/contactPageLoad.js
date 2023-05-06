bac871e44b61061814c9886dbc501c4c849f5b89
dwcarey <danielwilliamcarey@gmail.com>
2023-05-06 12:48:42 +0800

    finished contact page content and embedded google map

--- src/contactPageLoad.js
+++ src/contactPageLoad.js
@@ -1,36 +1,75 @@
-import meerkatImage from "./MeerkatImage.jpg";
+import contactsLion from "./contactsLion.jpg";
+
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of the jungle
+  const position = { lat: -3.077, lng: 35.626 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at the jungle 
+  map = new Map(document.getElementById("map"), {
+    zoom: 8,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at the jungle in Africa
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Jungle",
+  });
+};
+
 
 const contactPageLoad = (() => {
+
   const contentDiv = document.getElementById('content');
   contentDiv.replaceChildren();
-  
-
-  function imageComponent() {
-    const imageHolder = document.createElement('div');
-    imageHolder.id = 'imageHolder';
-    const meerkat = new Image();
-    meerkat.src = meerkatImage;
-    imageHolder.appendChild(meerkat);
-    return imageHolder;
-  }
 
   function headerComponent() {
     const headerHolder = document.createElement('h1');
     headerHolder.id = 'headerHolder';
-    headerHolder.textContent = 'Contacts';
+    headerHolder.textContent = 'Contact';
     return headerHolder;
+  }
+
+  function contactsImage() {
+    const image = document.createElement('img');
+    const lionHolder = document.createElement('div');
+    image.src = contactsLion;
+    lionHolder.classList.add('lionHolder');
+    lionHolder.appendChild(image);
+    return lionHolder;
   }
 
   function bodyComponent() {
     const bodyHolder = document.createElement('div');
     bodyHolder.id = 'bodyHolder';
-    //add contacts
+    const contactBlurb = document.createElement('p');
+    contactBlurb.textContent = `Welcome to The Jungle, located in the heart of Ngorongoro Conservation! If you have any questions, comments, or concerns, please do not hesitate to contact us. Our friendly staff are always happy to assist you and ensure that your dining experience is one to remember.
+
+    To find us, simply head to Ngorongoro Conservation and look for the large, brightly colored sign with our restaurant's name on it. We are situated in a lush and vibrant jungle setting, surrounded by the sights and sounds of nature. We can't wait to welcome you to our unique and unforgettable dining experience in the heart of the African wilderness!`
+    const mapDiv = document.createElement('div');
+    mapDiv.id = 'map';
+    mapDiv.style.height = '400px';
+    contactBlurb.id = 'contactBlurb';
+    bodyHolder.appendChild(contactBlurb);
+    bodyHolder.appendChild(mapDiv);
+    initMap();
     return bodyHolder;
   }
 
+    
   contentDiv.appendChild(headerComponent());
-  contentDiv.appendChild(imageComponent());
+  contentDiv.appendChild(contactsImage());
   contentDiv.appendChild(bodyComponent());
 });
 
 export { contactPageLoad };
+
